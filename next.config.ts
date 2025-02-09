9c4f814d5f8dcb689dfcb36af5c49c09be60d208
PerthTheIamCat <thongcum2546@gmail.com>
2025-02-09 18:13:08 +0700

    Add environment variables support and update Tailwind CSS colors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    PUSHER_APP_ID: process.env.PUSHER_APP_ID,
+    PUSHER_KEY: process.env.PUSHER_KEY,
+    PUSHER_SECRET: process.env.PUSHER_SECRET,
+    PUSHER_CLUSTER: process.env.PUSHER_CLUSTER,
+  },
 };
 
 export default nextConfig;
